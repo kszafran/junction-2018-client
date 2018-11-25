dd7aae7517a75ac11efbe0b71b10789e379b1313
Krzysztof Szafrański <k.p.szafranski@gmail.com>
2018-11-25 02:54:35 +0200

    Add app logo on top navigation bar

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'pathTraceDemo')));
+
+app.get('/network', function(req, res) {
+  res.sendFile(path.join(__dirname, 'pathTraceDemo', 'pathTraceDemo.htm'));
+});
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
